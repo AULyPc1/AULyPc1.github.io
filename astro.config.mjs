f0c048d7336f53b7143b0269508621c6861e599c
AULyPc <1448581334@qq.com>
2025-02-10 04:55:21 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,7 +42,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.aulypc0x0.online',
+  site: 'https://aulypc1.github.io',
   base: '/',
   trailingSlash: 'always',
   integrations: [
